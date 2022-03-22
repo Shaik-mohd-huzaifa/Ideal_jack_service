5cc6fdab5d55b6841e067106dbaf06a9a620c12b
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-03-22 22:24:57 +0530

    added formheightchanging function, navbar color changing function, dropquote function for dropdown form or shrink down the form

--- main.js
+++ main.js
@@ -47,4 +47,43 @@ function sendForm(){
   }
 }
 
+        // function which changes the height and content of the form header
 
+  function floatingFormHeight(){
+  const screenheight = window.innerHeight;
+  if(screenheight < 720){
+    document.getElementById("Name").style.background = "black";
+    document.getElementById("Form-header").color = "white";
+  }else{
+    document.getElementById("Name").style.background = "";
+  }
+}
+
+window.onresize = floatingFormHeight;
+ 
+document.getElementById("Form-header").style.color = "black";
+
+
+// function to change the color of the navbar when scrolling
+function changeColor(){
+  let navbar = document.getElementById("changing-navbar");
+  let scroll = document.getElementById("scroll");
+  let scrollTop = document.documentElement.scrollTop;
+  if(scrollTop > 50){
+    navbar.className = "active";
+    scroll.className = "active";
+  }else{
+    navbar.className = "unvisible-nav";
+    scroll.className = "unvisible-nav";
+  }
+}
+
+
+function dropquote(){
+  const dropcontainer = document.getElementById("drop-container");
+  if(dropcontainer.className === "drop-container"){
+    dropcontainer.className += " active";
+  }else{
+    dropcontainer.className = "drop-container";
+  }
+}
