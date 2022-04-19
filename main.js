8572b1755ae9596dd5e7c036ad577bcb83fbd23a
Shaik Mohammed Huzaifa <shaikmohdhuz@gmail.com>
2022-04-19 13:05:50 +0530

    Added and modified properties to be changed when toggle bar is clicked

--- main.js
+++ main.js
@@ -10,28 +10,58 @@
 }
 
 online();*/
-
 function display(){
-  let Navname = document.getElementById("changing-navbar");
-  console.log(Navname);
-  if(Navname.className === "unvisible-nav"){
-    Navname.className += " active";
-    document.getElementById("logoheader").style.display = "none";
-  }else{
-    Navname.className = "unvisible-nav";
-    document.getElementById("logoheader").style.display = "";
-  }
+let Mainynav = document.getElementById("main-navbar");
+ if(Mainynav.className === "navbar"){
+   Mainynav.className += " active";
+ }else{
+   Mainynav.className = "navbar";
+ }
 }
 
+  
+
+// function to activate side nav bar for responsive or mobile screens
 function changenav(){
-   let togglebar = document.getElementById("rotating-nav");
-   if(togglebar.className === "wrong-nav"){
-     togglebar.className += " active";
-   }else{
-     togglebar.className = "wrong-nav";
-   }
-     document.getElementById("main-nav-container").style.backgroundColor = "white";
-   
+   let rotatingbox = document.getElementById("rotating-container");
+   if(rotatingbox.className === "rotating-box"){
+    //  Adds the active to the class "rotating-box" 
+     rotatingbox.className += " active";
+
+    //  removes active when clicked again 
+     document.getElementById("nav-container").className += " active";
+     
+    //  removes the logo when clicked on toggle bar 
+     document.getElementById("logo-container").style.display = "none";
+
+    // Adds 100% width for the main-navbar  
+     document.getElementById("main-navbar").style.width = "100%";
+
+    // Adds the "sidenav" to main-nav-container class
+     document.getElementById("main-nav-container").className += " sidenav";
+     
+    // Adds overflow property as hidden when clicked 
+     document.querySelector("body").style.overflow = "hidden";
+
+    }else{
+    //  removes active when clicked again 
+     rotatingbox.className = "rotating-box";
+
+    //  removes active when clicked again 
+     document.getElementById("nav-container").className = "nav-container";
+
+    //  removes the display = "none" property and sets it as default 
+     document.getElementById("logo-container").style.display = "";
+
+    // removes the width = 100% property and sets to normal 
+     document.getElementById("main-navbar").style.width = "";
+
+    //  removes "sidenav" when clicked again
+     document.getElementById("main-nav-container").className = "main-nav-container";
+     
+    //  removes Overflow property   
+     document.querySelector("body").style.overflow = "";
+    } 
 }
 
 
@@ -89,6 +119,16 @@ function dropquote(){
   }else{
     dropcontainer.className = "drop-container";
   }
+} 
+
+
+function onlinechecker(){
+  const $online = navigator.onLine;
+if($online === true){
+
+}else{
+  
+}
 }
 
 // calculate the sum of two matrixes
